8730af3c3c22d8e275c3dd3c769eb1041ee3e9b8
Aurelien Turpin <aurelien.turpin@telecom-st-etienne.fr>
2018-05-25 11:47:53 +0200

    Oublie de sauvegarde

--- AlatacadabraQt/snake.h
+++ AlatacadabraQt/snake.h
@@ -1,39 +1,25 @@
 #ifndef SNAKE_H
 #define SNAKE_H
-<<<<<<< HEAD
-#include <vector>
-#include "bodypart.h"
-=======
 
 #include "bodypart.h"
 #include <vector>
->>>>>>> Modification du modèle, maintenant, on a une grille composée de node et dans cette grille on stocke le snake
 
 class Snake
 {
 private:
-<<<<<<< HEAD
-    Point direction_;
-    int size_;
-    Vector<BodyPart> body_;
-
-public:
-    Snake();
-    void drawSnake();
-    void move();
-    bool deadlyPLace();
-    void eatFruit();
-=======
     // liste des indices auxquels on a les points du snake
     vector<int> listNode_;
     // Diamètre des éléments du snake
     int radius_;
+    Point direction_;
+    int size_;
 
 public:
     Snake();
     void drawNode();
-
->>>>>>> Modification du modèle, maintenant, on a une grille composée de node et dans cette grille on stocke le snake
+    void move();
+    bool deadlyPLace();
+    void eatFruit();
 };
 
 #endif // SNAKE_H
