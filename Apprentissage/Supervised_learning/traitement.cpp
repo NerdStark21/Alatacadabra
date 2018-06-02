b06d3d5f438c2dfa39aebb5213eea9e70ad6524f
lynahiryuu <31628324+lynahiryuu@users.noreply.github.com>
2018-06-02 16:13:07 +0200

    Calcul avec itérateurs mais ne réduit pas le temps de calcul...

--- Apprentissage/Supervised_learning/traitement.cpp
+++ Apprentissage/Supervised_learning/traitement.cpp
@@ -66,14 +66,8 @@ VideoCapture Traitement::getVideoCapture(){
 
 int Traitement::getDirection(Mat frame){
 
-    //Mat frame; //Image courante
     Mat frameYCbCr; // Image dans le domaine YCbCr
     Mat frameBlack; //Image binaire
-
-
-    //Prendre une frame de la webCam
-    //webCam_ >> frame;
-
 
     //Conversion en YCbCr
     cv::cvtColor(frame,frameYCbCr, COLOR_BGR2YCrCb);
@@ -83,8 +77,6 @@ int Traitement::getDirection(Mat frame){
     //Segmentation
 
     bayesien *baye = new bayesien();
-
-    //Segmentation
 
     frameBlack = Segmentation(baye, frameYCbCr);
 
@@ -98,66 +90,50 @@ Mat Traitement::Segmentation(bayesien * baye, Mat frameYCbCr){
 
     Mat frameBlack; //Image binaire
 
-    //On crée une copie de frameYCbCr;
+    // Itérator qui parcourira tous les pixels de l'image qui sont de type CvScalar
+    MatIterator_<CvScalar> it;
+
+    //On crée une copie de frameYCbCr qui deviendra une image binaire
     frameYCbCr.copyTo(frameBlack);
 
-    //Images permettant de réaliser des traitements comme l'extraction d'un pixel.
-    IplImage tempBlack = frameBlack;
-    IplImage temp = frameYCbCr;
+    for (it = frameBlack.begin<CvScalar>(); it != frameBlack.end<CvScalar>(); it++){
 
-    //Pour chaque pixels de l'image, on réalise une classification bayésienne:
-    int row = frameYCbCr.rows;
-    int col = frameYCbCr.cols;
+        //cout<<"Je suis dans le for"<<endl;
 
-    for (int i = 0 ; i < row ; i++){
+        //Récupération de la valeur du pixel
+        CvScalar pix;
+        pix = *it;
 
-        for (int j = 0 ; j < col ; j++){
+        vector<float> pixel;
+        pixel.push_back(pix.val[0]);
+        pixel.push_back(pix.val[1]);
+        pixel.push_back(pix.val[2]);
 
-            //cout<<"Je suis dans le for"<<endl;
+        //Application de la règle bayésienne
 
+        if (baye->regle_bayesienne(pixel)){
 
-            //Récupération de la valeur du pixel
-            CvScalar pix;
+            //On met le pixel en blanc
+            pix.val[0] = 255;
+            pix.val[1] = 255;
+            pix.val[2] = 255;
 
+            //On l'insère dans l'image en noir et blanc
+            *it = pix;
+        }
+        else{
 
-            pix = cvGet2D(&temp, i, j);
+            //On met le pixel en noir
 
-            vector<float> pixel;
-            pixel.push_back(pix.val[0]);
-            pixel.push_back(pix.val[1]);
-            pixel.push_back(pix.val[2]);
+            pix.val[0] = 0;
+            pix.val[1] = 0;
+            pix.val[2] = 0;
 
-            //Application de la règle bayésienne
-
-            if (baye->regle_bayesienne(pixel)){
-
-                //On récupère le pixel qu'on veut changer dans la matrice binaire
-                pix = cvGet2D(&tempBlack, i, j);
-
-                //On met le pixel en blanc
-                pix.val[0] = 255;
-                pix.val[1] = 255;
-                pix.val[2] = 255;
-
-                //On l'insère dans l'image en noir et blanc
-                cvSet2D(&tempBlack,i,j,pix);
-            }
-            else{
-                //On récupère le pixel qu'on veut changer dans la matrice binaire
-                pix = cvGet2D(&tempBlack, i, j);
-
-                //On met le pixel en noir
-
-                pix.val[0] = 0;
-                pix.val[1] = 0;
-                pix.val[2] = 0;
-
-                //On l'insère dans l'image en noir et blanc
-                cvSet2D(&tempBlack,i,j,pix);
-
-            }
+            //On l'insère dans l'image en noir et blanc
+            *it = pix;
 
         }
+
     }
 
     return frameBlack;
